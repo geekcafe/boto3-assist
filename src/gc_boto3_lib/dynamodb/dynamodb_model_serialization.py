2d838b4941dcf4edf316363cfdfdfea6337bfe72
Eric WIlson <eric.wilson@geekcafe.com>
2024-07-24 18:45:00 -0400

    ✨ feat (serialization): 🧹 cleanup

--- src/gc_boto3_lib/dynamodb/dynamodb_model_serialization.py
+++ src/gc_boto3_lib/dynamodb/dynamodb_model_serialization.py
@@ -1,4 +1,3 @@
-from boto3.dynamodb.conditions import Key
 from boto3.dynamodb.types import TypeSerializer
 import inspect
 
@@ -17,45 +16,8 @@ class DynamoDbSerializer:
         Returns:
         - dict: A dictionary representation of the class instance suitable for DynamoDB client.
         """
-
         serializer = TypeSerializer()
-
-        def serialize_value(value):
-            """
-            Serialize the value to DynamoDB format.
-            """
-            if is_primitive(value):
-                return serializer.serialize(value)
-            elif isinstance(value, list):
-                return serializer.serialize([serialize_value(v) for v in value])
-            elif isinstance(value, dict):
-                return serializer.serialize(
-                    {k: serialize_value(v) for k, v in value.items()}
-                )
-            else:
-                return serializer.serialize(
-                    DynamoDbSerializer.to_client_dictionary(value)
-                )
-
-        def is_primitive(value):
-            """
-            Check if the value is a primitive data type.
-            """
-            return isinstance(value, (str, int, float, bool, type(None)))
-
-        instance_dict = {}
-        # Add instance variables
-        for attr, value in instance.__dict__.items():
-            instance_dict[attr] = serialize_value(value)
-
-        # Add properties
-        for name, method in inspect.getmembers(
-            instance.__class__, predicate=inspect.isdatadescriptor
-        ):
-            if isinstance(getattr(instance.__class__, name), property):
-                instance_dict[name] = serialize_value(getattr(instance, name))
-
-        return {k: v for k, v in instance_dict.items()}
+        return DynamoDbSerializer._serialize(instance, serializer.serialize)
 
     @staticmethod
     def to_resource_dictionary(instance: object):
@@ -68,36 +30,47 @@ class DynamoDbSerializer:
         Returns:
         - dict: A dictionary representation of the class instance suitable for DynamoDB resource.
         """
+        return DynamoDbSerializer._serialize(instance, lambda x: x)
 
-        def serialize_value(value):
-            """
-            Serialize the value to a format suitable for DynamoDB resource.
-            """
-            if is_primitive(value):
-                return value
-            elif isinstance(value, list):
-                return [serialize_value(v) for v in value]
-            elif isinstance(value, dict):
-                return {k: serialize_value(v) for k, v in value.items()}
-            else:
-                return DynamoDbSerializer.to_resource_dictionary(value)
-
+    @staticmethod
+    def _serialize(instance: object, serialize_fn):
         def is_primitive(value):
-            """
-            Check if the value is a primitive data type.
-            """
+            """Check if the value is a primitive data type."""
             return isinstance(value, (str, int, float, bool, type(None)))
 
+        def serialize_value(value):
+            """Serialize the value using the provided function."""
+            if is_primitive(value):
+                return serialize_fn(value)
+            elif isinstance(value, list):
+                return serialize_fn([serialize_value(v) for v in value])
+            elif isinstance(value, dict):
+                return serialize_fn({k: serialize_value(v) for k, v in value.items()})
+            else:
+                return serialize_fn(DynamoDbSerializer._serialize(value, serialize_fn))
+
+        instance_dict = DynamoDbSerializer._add_properties(instance, serialize_value)
+        return instance_dict
+
+    @staticmethod
+    def _add_properties(instance: object, serialize_value) -> dict:
         instance_dict = {}
         # Add instance variables
         for attr, value in instance.__dict__.items():
-            instance_dict[attr] = serialize_value(value)
+            # don't ge the private properties
+            if not str(attr).startswith("_"):
+                if value is not None:
+                    instance_dict[attr] = serialize_value(value)
 
         # Add properties
-        for name, method in inspect.getmembers(
+        for name, _ in inspect.getmembers(
             instance.__class__, predicate=inspect.isdatadescriptor
         ):
             if isinstance(getattr(instance.__class__, name), property):
-                instance_dict[name] = getattr(instance, name)
+                # don't ge the private properties
+                if not str(name).startswith("_"):
+                    value = getattr(instance, name)
+                    if value is not None:
+                        instance_dict[name] = serialize_value(value)
 
         return instance_dict
