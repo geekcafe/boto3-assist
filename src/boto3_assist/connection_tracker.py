a785cc801a779a1f1daa4db0d9e7e12724fc2cdd
Eric WIlson <eric.wilson@geekcafe.com>
2024-11-28 19:05:05 -0500

    ✨ feat (connection): tracking

--- src/boto3_assist/connection_tracker.py
+++ src/boto3_assist/connection_tracker.py
@@ -15,16 +15,23 @@ class ConnectionTracker:
     """
 
     def __init__(self, service_name: str) -> None:
-        self.__stack_trace_env_var: str = "ISSUE_STACK_TRACE"
-        self.__connection_couter: int = 0
+        self.__stack_trace_env_var: str = "BOTO3_ASSIST_CONNECTION_STACK_TRACE"
+        self.__connection_counter: int = 0
         self.__service_name: str = service_name
-        self.__issue_stack_trace: bool = (
-            os.getenv(f"{self.__stack_trace_env_var}", "false") == "true"
-        )
+        self.__issue_stack_trace: bool | None = None
+
+    @property
+    def issue_stack_trace(self) -> bool:
+        """Returns True if the stack trace should be issued"""
+        if self.__issue_stack_trace is None:
+            self.__issue_stack_trace = (
+                os.getenv(self.__stack_trace_env_var, "").lower() == "true"
+            )
+        return self.__issue_stack_trace
 
     def increment_connection(self) -> None:
         """Increments the connection counter"""
-        self.__connection_couter += 1
+        self.__connection_counter += 1
 
         if self.connection_count > 1:
             service_message = ""
@@ -32,40 +39,41 @@ class ConnectionTracker:
             if self.__service_name:
                 service_message = f"Your {self.__service_name} service has more than one connection.\n"
 
-            if not self.__issue_stack_trace:
+            if not self.issue_stack_trace:
                 stack_trace_message = (
                     f"\nTo add addtional information to the log and determine where additional connections are being created"
                     f", set the environment variable {self.__stack_trace_env_var} to true.\n"
                 )
+            else:
+                stack = "\n".join(traceback.format_stack())
+                stack_trace_message = (
+                    f"\nStack Trace Enabeld with {self.__stack_trace_env_var}"
+                    f"\n{stack}"
+                )
+
             self.__log_warning(
                 f"{service_message}"
-                f"Your dynamodb connection count is {self.connection_count}. "
+                f"Your boto3 connection count is {self.connection_count}. "
                 "Under most circumstances you should be able to use the same connection "
                 "vs. creating a new one.  Connections are expensive in terms of time / latency. "
                 "If you are seeing perforance issues, check how and where you are creating your "
-                "connections.  You should be able to pass the .db connection to your other objects "
-                "and reuse your dynamodb boto connections."
+                "connections.  You should be able to pass the connection to your other objects "
+                "and reuse your boto3 connections."
                 f"{stack_trace_message}"
             )
 
-            # do a stack trace
-            if self.__issue_stack_trace:
-                print("Stack Trace")
-                traceback.print_stack()
-                print("")
-
     def decrement_connection(self) -> None:
         """Decrements the connection counter"""
-        self.__connection_couter -= 1
+        self.__connection_counter -= 1
 
     @property
     def connection_count(self) -> int:
         """Returns the current connection count"""
-        return self.__connection_couter
+        return self.__connection_counter
 
     def reset(self) -> None:
         """Resets the connection counter"""
-        self.__connection_couter = 0
+        self.__connection_counter = 0
 
     def __log_warning(self, message: str) -> None:
         """Logs a warning message"""
