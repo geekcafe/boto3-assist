edd41d781ccb52aa2324ab9be5cdef64acbcf440
Eric WIlson <eric.wilson@geekcafe.com>
2024-12-07 22:13:43 -0500

    🧹 cleanup

--- src/boto3_assist/cloudwatch/cloudwatch_connection.py
+++ src/boto3_assist/cloudwatch/cloudwatch_connection.py
@@ -8,10 +8,8 @@ from typing import Optional
 from typing import TYPE_CHECKING
 
 from aws_lambda_powertools import Logger
-from boto3_assist.boto3session import Boto3SessionManager
-from boto3_assist.environment_services.environment_variables import (
-    EnvironmentVariables,
-)
+
+
 from boto3_assist.cloudwatch.cloudwatch_connection_tracker import (
     CloudWatchConnectionTracker,
 )
