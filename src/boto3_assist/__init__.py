4cc7af9662a2b023094d5a9803db616712628e96
Eric Wilson <eric.wilson@aplosanalytics.com>
2026-03-25 17:26:23 -0400

    💬 comment/log (version): library name and version info in logs

--- src/boto3_assist/__init__.py
+++ src/boto3_assist/__init__.py
@@ -6,5 +6,8 @@ MIT License. See Project Root for the license information.
 
 from .connection import Connection
 from .connection_pool import ConnectionPool
+from .version import __version__
 
 __all__ = ["Connection", "ConnectionPool"]
+
+print(f"✨ Boto3 Assist - Open Source Boto3 Assist Tool v{__version__}")
